6e5d145f05869845c64066a5b2b97fdb93865582
Shrey Thakkar <thakkarshrey10.st@gmail.com>
2024-09-10 00:49:10 +0530

    tried to create the octagon as button so that on clicking it; it rolls in the middle of screen and opens up its menu

--- animated-octagon-menu/index.js
+++ animated-octagon-menu/index.js
@@ -1,8 +1,16 @@
 window.addEventListener("DOMContentLoaded", function () {
   let menu_toggle = document.querySelector(".menu-toggle");
+  let octagon_toggle = this.document.querySelector(".octagon");
   let menu = document.querySelector(".menu");
 
-  menu_toggle.onclick = () => {
+  octagon_toggle.onclick = () => {
     menu.classList.toggle("active");
+    menu.classList.toggle("centered");
+    octagon_toggle.classList.toggle("centered");
+  };
+  menu.onclick = () => {
+    menu.classList.toggle("active");
+    menu.classList.toggle("centered");
+    octagon_toggle.classList.toggle("centered");
   };
 });
