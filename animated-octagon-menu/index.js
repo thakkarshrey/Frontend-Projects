2640d8eecd5bb421d04e4f9ed84fbf71773d6e65
Shrey Thakkar <thakkarshrey10.st@gmail.com>
2024-09-11 01:35:32 +0530

    added an after in the octagon menu and added a blur effect as well. Also placed the menu inside navbar

--- animated-octagon-menu/index.js
+++ animated-octagon-menu/index.js
@@ -1,16 +1,9 @@
 window.addEventListener("DOMContentLoaded", function () {
-  let menu_toggle = document.querySelector(".menu-toggle");
-  let octagon_toggle = this.document.querySelector(".octagon");
   let menu = document.querySelector(".menu");
+  let menu_container = document.querySelector(".menu-container");
 
-  octagon_toggle.onclick = () => {
-    menu.classList.toggle("active");
-    menu.classList.toggle("centered");
-    octagon_toggle.classList.toggle("centered");
-  };
   menu.onclick = () => {
     menu.classList.toggle("active");
-    menu.classList.toggle("centered");
-    octagon_toggle.classList.toggle("centered");
+    menu_container.classList.toggle("active");
   };
 });
